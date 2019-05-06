3c972b5b551267a6c9fbfc3866dce1606b7d322b
3144207329 <3144207329@qq.com>
2019-05-06 10:06:37 +0800

    保存

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,5 @@ public class HelloWorld {
 		System.out.println("hello world");
 		System.out.println("修改了HelloWorld类");
 		System.out.println("第二次修改");
-		System.out.println("在Develop分支下输出一条信息");
 	}
 }
